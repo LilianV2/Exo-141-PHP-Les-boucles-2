d2cd4a1c1319629be75a91ff41fb33eca0d6084b
lilia <lilianom59@outlook.fr>
2023-01-11 16:46:45 +0100

    loop

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+// EXO 1
+$i = 0;
+
+for($i;$i<10;$i++){
+   echo $i . "<br>";
+}
+echo "<br>";
+
+// EXO 2
+$a = 0;
+$b = 22;
+
+for($a;$a<20;$a++){
+    echo $a * $b . "<br>";
+}
+echo "<br>hdrfvyjdrg";
+
+
+$c = 100;
+$d = 54;
+
+while($c <= 10){
+    echo $c *= $d;
+    $c--;
+}
+
+
+// EXO 4
+$e = 1;
+
+while($e < 10){
+    echo "<br>" .  $e += 0.5;
+}
+echo "<br>";
+
+// EXO 5
+for ($x = 0;$x < 15; $x++){
+    echo "On y arrive presque<br>";
+}
+echo "<br>";
+
+//EXO 6
+for ($x = 20;$x > 0; $x--){
+    echo "c'est presque bon<br>";
+}
+echo "<br>";
+
+//EXO 7
+for ($x = 1;$x < 100; $x += 15){
+    echo "on tient le bon bout<br>";
+}
+echo "<br>";
+
+//EXO 8
+for ($x = 200;$x > 0; $x -= 12){
+    echo "Enfin!!!!<br>";
+}
+echo "<br>";
